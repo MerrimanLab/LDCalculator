0571f5ca0ea3c1502c3d7f26be451193e97b68aa
nickb- <nick.dale.burns@gmail.com>
2015-12-15 15:14:57 +1300

    UI designed.

---
+++ server.R
@@ -0,0 +1,15 @@
+##
+## LD Calculation Interface
+##
+## This interface allows users to define the input parameters for
+## James's LD Calculation script and dynamically builds the command string.
+##
+## Nick Burns
+## Dec 2015
+##
+
+## Server.R
+library(shiny)
+setwd("~/Google Drive/OtagoARF/LDCalcInterface")
+
+shinyServer(function(input, output) {})
---
+++ ui.R
@@ -0,0 +1,115 @@
+##
+## LD Calculation Interface
+##
+## This interface allows users to define the input parameters for
+## James's LD Calculation script and dynamically builds the command string.
+##
+## Nick Burns
+## Dec 2015
+##
+
+## UI.R
+library(shiny)
+setwd("~/Google Drive/OtagoARF/LDCalcInterface")
+
+
+shinyUI(
+  
+    fluidPage(
+      
+        h1("LD Calculation",
+           style = "font-family: 'Arial'; text-align: center;"),
+        hr(),
+        hr(),
+        
+        # File input widgets
+        #     - inputVCFFile: user VCF file
+        #     - inputSNPFile: text file conatining snp RSID to calclute LD for
+        p(
+            "Select an input file. Input files may be either a user-defined VCF file or,
+            a text file containing a list of target SNPs."
+        ),
+        p(
+            "Additionally, you can enter a list of SNPs to include / exclude as required (';' delimited)"
+        ),
+        br(),
+        fluidRow(
+          # InputVCFFile    option: -v
+          column(4,
+                 wellPanel (
+                   fileInput("inputVCFFile", label=h4("VCF File Input"))
+                 )
+          ),
+          # InputSNPFile    option: -s
+          column(4, 
+                 wellPanel (
+                   fileInput("inputSNPFile", label=h4("SNP File Input"))
+                 )
+          ),
+          # Individual SNPs    options: -r, -i
+          column(4, 
+                 wellPanel (
+                   textInput("textSNPInclude", label=p("SNPs to include...")),
+                   textInput("textSNPExclude", label=p("SNPs to exclude..."))
+                 )
+          )
+        ),
+        br(),
+        hr(),
+        br(),
+        
+        # Window of Calculation
+        #     windowSNPs: the LD distance in number of SNPs
+        #     windowKB:   the maximum distance (Kb) over which to calculate LD
+        #     windowFreq: only show SNPs with LD greater than this
+        fluidRow(
+          # windowSNPs   option: -w
+          column(4, sliderInput("sliderWindowSNPs", 
+                               label=p("LD Distance in number of SNPs"),
+                               min = 0, max = 100, value = 0)
+          ),
+          # windowKB    option: -l
+          column(4, sliderInput("sliderWindowKB", 
+                               label=p("Max distance (Kb) to calculate LD"),
+                               min = 0, max = 1000, value = 200)
+          ),
+          # windowFreq    option: -r
+          column(4, sliderInput("sliderWindowFreq", 
+                               label=p("Only show SNPs with LD greater than:"),
+                               min = 0, max = 1, value = 0)
+          )
+        ),
+        br(),
+        hr(),
+        br(),
+        
+        # other options
+        #     textHaploview: text input with the filename for haploview output.
+        #     textPED:       text input with the filename for haploview output.
+        #     checkMatrix:   Output data in a matrix
+        #     textLogFile:   text input with the filename for log file
+        fluidRow(
+          # textHaploview   option: -h
+          column(4, textInput("textHaploview", label=p("Haploview output:"))),
+                 
+          # textPED    option: -p
+          column(4, textInput("textPED", label=p("PED file output:"))),
+                 
+          # checkMatrix    option: -m
+          column(4, checkboxGroupInput("checkMatrix", 
+                                        label=p("Output data to matrix"),
+                                       choices = list("No" = 1, "Yes" = 2),
+                                        selected = 1)
+          )
+        ),
+        br(),
+        hr(),
+        br(),
+        
+        # Submit Button
+        fluidRow(
+          column(4, submitButton(text = h4("Calculate Now")))
+        )
+        
+    )
+)
